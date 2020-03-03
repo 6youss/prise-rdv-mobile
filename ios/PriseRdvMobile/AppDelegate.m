ea4e976affbd19ef89cc2320f66bccb6bdfa317d
Youcef ZAHARIOU <youcef.zahariou@gmail.com>
2020-03-03 14:53:15 +0100

    added the appcenter crash analytics to ios version

--- ios/PriseRdvMobile/AppDelegate.m
+++ ios/PriseRdvMobile/AppDelegate.m
@@ -4,6 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 #import <Firebase.h>
 #import "AppDelegate.h"
 
@@ -15,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   
   if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
